bd777f785f4ba1ccc74a48f25f1c48d4dbdcb6ea
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 06:13:45 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,13 +1,3 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dancer.rb'
-class Kid
-  include Dance
-  extend MetaDancing
-  include FancyDance::InstanceMethods
-  extend FancyDance::ClassMethods
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-  end
-end
