d612212a3894e95f7318e0047edfb9aa1faf6ae3
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 06:13:40 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,11 +3,3 @@ require_relative './dance_module.rb'
 require_relative './fancy_dancer.rb'
 
 
-class Dancer
-  include Dance       #=> "include" - These are instance methods
-  extend MetaDancing  #=> "extend"  - These are class methods
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-  end
-end
